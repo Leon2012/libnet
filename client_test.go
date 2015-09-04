1b6c8cab0734138be2b44280d631636b5a31fb0b
PengLeon <leon.peng@live.com>
2015-09-04 22:54:46 +0800

    commit

---
+++ client_test.go
@@ -0,0 +1,38 @@
+package libnet
+
+import (
+	"fmt"
+	"io"
+	"testing"
+)
+
+type MyClientCodec struct {
+}
+
+func (m *MyClientCodec) Encode(message []byte) ([]byte, error) {
+	return message, nil
+}
+
+func (m *MyClientCodec) Decode(r io.Reader) ([]byte, error) {
+	buf := make([]byte, 1024)
+	if _, err := r.Read(buf); nil != err {
+		return nil, err
+	}
+	return buf, nil
+}
+
+func TestClient(t *testing.T) {
+	addr := "127.0.0.1:3333"
+	client := NewTcpClient(addr, &MyClientCodec{}, 0, 0)
+	err := client.Connect()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		n, _ := client.Send([]byte("hello\n"))
+		fmt.Printf("send data length : %d \n", n)
+
+		data, _ := client.Recv()
+		fmt.Println("recv:" + string(data))
+		client.Close()
+	}
+}
--- tcp_client.go
+++ tcp_client.go
@@ -2,21 +2,23 @@ package libnet
 
 import (
 	"net"
+	"time"
 )
 
 type TcpClient struct {
-	addr         string
-	trans        Transport
-	transHandler TransportHandler
-	codec        Codec
+	addr               string
+	trans              Transport
+	codec              Codec
+	rTimeout, wTimeout time.Duration
 }
 
-func NewTcpClient(addr string, th TransportHandler, c Codec) *TcpClient {
+func NewTcpClient(addr string, c Codec, rTimeout, wTimeout int) *TcpClient {
 	return &TcpClient{
-		addr:         addr,
-		transHandler: th,
-		codec:        c,
-		trans:        nil,
+		addr:     addr,
+		codec:    c,
+		trans:    nil,
+		rTimeout: time.Duration(rTimeout) * time.Second,
+		wTimeout: time.Duration(wTimeout) * time.Second,
 	}
 }
 
@@ -26,8 +28,35 @@ func (t *TcpClient) Connect() error {
 		return err
 	}
 
-	conn, err := net.Dial("tcp", tcpAddr.String())
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	if err != nil {
 		return err
 	}
+
+	t.trans = NewTCPTransport(conn, t.rTimeout, t.wTimeout)
+	t.trans.Conn().SetDeadline(time.Now().Add(1e9))
+	return nil
+}
+
+func (t *TcpClient) Send(message []byte) (int, error) {
+	buf, err := t.codec.Encode(message)
+	if err != nil {
+		return 0, err
+	} else {
+		return t.trans.Write(buf)
+	}
+}
+
+func (t *TcpClient) Recv() ([]byte, error) {
+	buf, err := t.codec.Decode(t.trans.Conn())
+	if err != nil {
+		return nil, err
+	} else {
+		return buf, nil
+	}
+}
+
+func (t *TcpClient) Close() {
+	t.trans.Conn().SetDeadline(time.Now())
+	t.trans.Close()
 }
